f20072954356fa1241d9337315f61a69a53c7c36
Rebecca Pizano <rebeccapizano@users.noreply.github.com>
2016-12-21 01:34:39 -0800

    Add files via upload

---
+++ CSharp/SimplePrograms/SimpleLoop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentCoTest
+{
+    class SimpleLoop
+    {
+       static void StairCase(int n)
+        {
+            for (int i=0; i<n; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            StairCase(6);
+        }
+    }
+}
