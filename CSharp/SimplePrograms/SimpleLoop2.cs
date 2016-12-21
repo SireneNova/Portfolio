a658eea4bdd3832d3804eb70ec2deb021ced2e62
Rebecca Pizano <rebeccapizano@users.noreply.github.com>
2016-12-21 02:19:23 -0800

    Update SimpleLoop2.cs

--- CSharp/SimplePrograms/SimpleLoop2.cs
+++ CSharp/SimplePrograms/SimpleLoop2.cs
@@ -1,1 +1,20 @@
+using System;
 
+namespace Test
+{
+    class SimpleLoop
+    {
+        static void Staircase(int n)
+        {
+            for (string i = ""; i.Length <= n; i = i + "#")
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            Staircase(6);
+        }
+    }
+}
