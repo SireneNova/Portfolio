02034b2329c236d0a3eb7f152f153b62931b8d84
Rebecca Pizano <rebeccapizano@users.noreply.github.com>
2016-12-25 14:50:11 -0800

    Update and rename CSharp/SimpleFunctions/Counting.cs to CSharp/SimpleFunctions/SimpleLoops/Counting.cs

---
+++ CSharp/SimpleFunctions/SimpleLoops/Counting.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Counting
+{
+
+    internal static void Count(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+}
