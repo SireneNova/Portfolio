2d646624235ed40b19ea27de798dfa330ee05808
Rebecca Pizano <rebeccapizano@users.noreply.github.com>
2016-12-25 16:07:28 -0800

    Update and rename CSharp/SimpleFunctions/Stairs.cs to CSharp/SimpleFunctions/SimpleLoops/Stairs.cs

--- CSharp/SimpleFunctions/SimpleLoops/Stairs.cs
+++ CSharp/SimpleFunctions/SimpleLoops/Stairs.cs
@@ -4,16 +4,13 @@ namespace Test
 {
     class Stairs
     {
-        static void Staircase(int n)
+        internal static void Staircase(int n)
         {
             for (string i = ""; i.Length <= n; i = i + "#")
             {
                 Console.WriteLine(i);
             }
         }
-        static void Main(string[] args)
-        {
-            Staircase(6);
-        }
+        
     }
 }
