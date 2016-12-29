c4dd099cc8e1e09a1bc4b70955887287c3b99430
Rebecca Pizano <rebeccapizano@users.noreply.github.com>
2016-12-28 20:52:05 -0800

    Update ConsoleDrill.cs

--- CSharp/FileTransferProgram/ConsoleDrill.cs
+++ CSharp/FileTransferProgram/ConsoleDrill.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.IO;
 
 
@@ -10,40 +6,37 @@ namespace ConsoleDrillFileMove
 {
     class FileMove
     {
-        static void Main()
+        static void move(string s, string d)
         {
             // my directories
-            string source = @"C:\path to source directory";
-            string destination = @"C:\path to destination directory";             
-                        
+            string source = s;
+            string destination = d;
+
             string[] fileArray = Directory.GetFiles(source, "*.txt"); //my criteria: get files if ends in .txt
             // Output of fileArray is in file paths rather than names.
 
             int count = 0;
-            string a = "\n*************************( Check Complete )*************************";
-            string b = "********************************************************************";
-
             foreach (string srcPath in fileArray)
             {
                 // Use Path class to manipulate file and directory paths.
                 string file = Path.GetFileName(srcPath);
-                string dstPath = System.IO.Path.Combine(destination, file);
-                //Console.WriteLine($"src: {srcPath}");
-                //Console.WriteLine($"dst: {dstPath}");
+                string dstPath = Path.Combine(destination, file);
                 DateTime mtime = File.GetLastWriteTime(srcPath); // Get time modified in local time
-                //Console.WriteLine(mtime);
                 TimeSpan diff = DateTime.Now - mtime;
                 double diffSeconds = diff.TotalSeconds;
+
                 if (diffSeconds <= 86400) // Move if modified within 24 h.
                 {
-                    //Console.WriteLine(diffSeconds);
-                    System.IO.File.Move(srcPath, dstPath);
+                    File.Move(srcPath, dstPath);
                     Console.WriteLine($"Change made to {file} in last 24 hr. File moved from source to destination.");
 
                     count += 1;
                 }
-                              
             }
+
+            //Message what's been moved
+            string a = "\n*************************( Check Complete )*************************";
+            string b = "********************************************************************";
             if (count > 1)
             {
                 Console.WriteLine(a);
@@ -64,7 +57,13 @@ namespace ConsoleDrillFileMove
                 Console.WriteLine("No files have been moved into the destination directory.");
                 Console.WriteLine(b);
             }
-            
+        }
+
+        static void Main()
+        {
+            string folderToCheck = @"C:\Users\Student\OneDrive\Documents\TechAcademy\10C#\Drills\CSharpConsoleDrill46\Source";
+            string foldertoSend = @"C:\Users\Student\OneDrive\Documents\TechAcademy\10C#\Drills\CSharpConsoleDrill46\Destination";
+            move(folderToCheck, foldertoSend);            
         }
     }
 }
