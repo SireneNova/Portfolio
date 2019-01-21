f41b227a71c12b7e5686ca326127d8db8a615937
Rebecca Pizano <rebeccapizano@users.noreply.github.com>
2019-01-21 09:01:15 -0800

    Update Delegates.cs

--- CSharp/ConsoleApps/Delegates.cs
+++ CSharp/ConsoleApps/Delegates.cs
@@ -3,13 +3,17 @@
 namespace Delegate
 {
     //Delegate: internal by default, type-safe function pointer
-    //can be used to point to functions with a similar syntax, such as the 
-    //way static void main does
+    //can be used to point to functions with a similar syntax, such as the way static void main does
    //This can point to any function with a void type and takes in a string parameter
    public delegate void HelloFunctionDelegate(string Message);
 
    class Program
     {
+       public static void Hello(string strMsg)
+        {
+            Console.WriteLine(strMsg);
+        }
+       
         public static void Main()
         {
             //Can create a new instance of the delegate as you would for a class.
@@ -18,13 +22,7 @@ namespace Delegate
 
             //Use the funtion you point to this way:
             del("Hello from delegate");
-
-        }
-        
-        public static void Hello(string strMsg)
-        {
-            Console.WriteLine(strMsg);
-        }
+        }    
 
         //Delegates are type safe because the delegate type signature 
         //must match the signature of the function it points to.
