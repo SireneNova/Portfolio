39ab5ebe3c1e244fe979bc10426b4c7e543c92a5
Sirene Nova <12014557+SireneNova@users.noreply.github.com>
2021-04-03 21:35:01 -0700

    Update Socks.cs

--- CSharp/PracticeFunctions/HackerRank/Dictionaries/Socks.cs
+++ CSharp/PracticeFunctions/HackerRank/Dictionaries/Socks.cs
@@ -1,39 +1,40 @@
 public class Program
 {
-	public static int sockMerchant(int[] ar) {
-        Dictionary<int, int> socks = new Dictionary<int, int>();
-        foreach(int color in ar)
-        {
-            if (socks.ContainsKey(color))
-            {
-                socks[color]+=1;
-            }
-            else{
-                socks[color]=1;
-            }
-        }
-        
-        Dictionary<int, int> pairs = new Dictionary<int, int>();
-        foreach(KeyValuePair<int, int> color in socks)
-        {
-            int key  = color.Key;
-            int numColors = socks[color.Key];
-            int numPairs = (int)numColors/2;
-            pairs.Add(key, numPairs);
-        }
-        
-        int sumPairs = 0;
-        foreach(KeyValuePair<int, int> pair in pairs)
-        {
-            sumPairs+=pair.Value;
-        }
-        
-        return sumPairs;
-    }
+	public static int sockMerchant(int[] ar) 
+	{
+		Dictionary<int, int> socks = new Dictionary<int, int>();
+		foreach(int color in ar)
+		{
+		    if (socks.ContainsKey(color))
+		    {
+			socks[color]+=1;
+		    }
+		    else{
+			socks[color]=1;
+		    }
+		}
+
+		Dictionary<int, int> pairs = new Dictionary<int, int>();
+		foreach(KeyValuePair<int, int> color in socks)
+		{
+		    int key  = color.Key;
+		    int numColors = socks[key];
+		    int numPairs = (int)numColors/2;
+		    pairs.Add(key, numPairs);
+		}
+
+		int sumPairs = 0;
+		foreach(KeyValuePair<int, int> pair in pairs)
+		{
+		    sumPairs+=pair.Value;
+		}
+
+		return sumPairs;
+	}
 
 	public static void Main()
 	{
-		int [] ar = {1,2,3,1,2,3,1,1,3,3};
+		int [] ar = {1,2,3,1,2,3,1,1,3,3,4};
 		Console.WriteLine(sockMerchant(ar));
 	}
 }
