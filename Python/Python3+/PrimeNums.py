33aba5a51d868733059ddcf3ecc391a7ace00168
Sirene Nova <12014557+SireneNova@users.noreply.github.com>
2021-04-07 15:16:46 -0700

    Update PrimeNums.py

--- Python/Python3+/PrimeNums.py
+++ Python/Python3+/PrimeNums.py
@@ -4,4 +4,4 @@ for n in range(2, 15):
         print(str(n) + " is a prime number")
     for x in range(2,n):    
         if n%x==0:
-            print(str(n) + " equals " + str(int(x)) + " X " + str(n//x)) #double slash is integer divisionso that decimal places .0 are not printed. could also cast to int. 
+            print(str(n) + " equals " + str(int(x)) + " X " + str(n//x)) #double slash is floor division so that decimal places '.0' are not printed. could also cast to int or rounded for same result. 
