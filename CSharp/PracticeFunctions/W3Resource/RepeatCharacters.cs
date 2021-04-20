ea4598e64239fbe40870a3860897c0b2e9645502
Sirene Nova <12014557+SireneNova@users.noreply.github.com>
2021-04-20 00:11:28 -0700

    Update and rename CSharp/PracticeFunctions/W3Resource/BasicExercises/RepeatCharacters.cs to CSharp/PracticeFunctions/W3Resource/RepeatCharacters.cs

--- CSharp/PracticeFunctions/W3Resource/RepeatCharacters.cs
+++ CSharp/PracticeFunctions/W3Resource/RepeatCharacters.cs
@@ -5,7 +5,7 @@ public class RepeatCharacters
     public static string repChar(string str)
     {
         return str.Length > 1 ?
-            str.Substring(0, 1)+
+            str.Substring(0, 1) +
             str +
             str.Substring(0, 1)
             : str;
