360a9d00df992e0d7a52cd5db94869c4addf4ff4
Sirene Nova <12014557+SireneNova@users.noreply.github.com>
2021-04-22 01:13:05 -0700

    Create Day2-Operators.cs

---
+++ CSharp/PracticeFunctions/HackerRank/30DaysCode/Day2-Operators.cs
@@ -0,0 +1,32 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the solve function below.
+    static void solve(double meal_cost, int tip_percent, int tax_percent) {
+        double totalCost = meal_cost * (1 + (double)tip_percent/100 + (double)tax_percent/100);
+        Console.WriteLine(Math.Round(totalCost));
+    }
+
+    static void Main(string[] args) {
+        double meal_cost = Convert.ToDouble(Console.ReadLine());
+
+        int tip_percent = Convert.ToInt32(Console.ReadLine());
+
+        int tax_percent = Convert.ToInt32(Console.ReadLine());
+
+        solve(meal_cost, tip_percent, tax_percent);
+    }
+}
