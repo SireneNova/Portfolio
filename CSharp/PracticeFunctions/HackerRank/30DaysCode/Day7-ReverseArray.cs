55f211b756a02f28f69cbd0fba050ff3bb84213b
Sirene Nova <12014557+SireneNova@users.noreply.github.com>
2021-04-22 01:42:42 -0700

    Create Day7-ReverseArray.cs

---
+++ CSharp/PracticeFunctions/HackerRank/30DaysCode/Day7-ReverseArray.cs
@@ -0,0 +1,32 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+    
+    static void reverse(int n, int[] A)
+    {        
+        for (int i = n-1; i>=0; i--)
+        {
+            Console.Write(A[i] + " ");           
+        }        
+    }
+
+    static void Main(string[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        
+        reverse(n, arr);
+    }
+}
