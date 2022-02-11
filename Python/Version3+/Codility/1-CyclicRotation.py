c678d00a79263fa1a72e734ee79b9141c001eee5
Sirene R Nova <12014557+SireneNova@users.noreply.github.com>
2022-02-10 21:35:59 -0800

    Update 1-CyclicRotation.py

--- Python/Version3+/Codility/1-CyclicRotation.py
+++ Python/Version3+/Codility/1-CyclicRotation.py
@@ -1,7 +1,7 @@
-#Write a function that rotates an array A clockwise by K spaces. This got %100. 
-#The tricky part for me was dealing with input larger than N, which is dealt with by the second conditional. 
-#I also learned that in Python, equality is set by reference by default. Setting B=A made B identical to A in memory, which is why .copy() was needed in this method.
-#There are probably other ways to do this, but this is what I came up with.
+# Write a function that rotates an array A clockwise by K spaces. This got %100. 
+# The tricky part for me was dealing with input larger than N, which is dealt with by the second conditional. 
+# I also learned that in Python, equality is set by reference by default for arrays. Setting B=A made B identical to A in memory, which is why .copy() was needed in this method.
+# There are probably other ways to do this, but this is what I came up with.
 
 def solution(A, K):
     N = len(A)
