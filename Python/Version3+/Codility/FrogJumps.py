e2c66df245a9e311b5abe9cf2c4044b839afedca
Sirene R Nova <12014557+SireneNova@users.noreply.github.com>
2022-02-10 21:29:38 -0800

    Create FrogJumps.py

---
+++ Python/Version3+/Codility/FrogJumps.py
@@ -0,0 +1,12 @@
+# A frog wants to get from point X to point Y, and jumps a fixed distance of D. Find the minimum number of frog jumps given starting point X, end point Y, and jump size D.
+# This solution got 100%. There are a number of other ways to do this.
+# I wasn't sure if codility would let me import the math module, so commented another solution that also seems to work. 
+
+import math
+
+def solution(X, Y, D):
+    diff = Y-X
+    minJumps = math.ceil(diff / D)
+    #remainder = (diff % D) / D
+    #minJumps = int(actualJumps + (1-remainder))
+    return minJumps
