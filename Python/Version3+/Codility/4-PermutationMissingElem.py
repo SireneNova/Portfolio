f4a22ceae5764f229562151e092979b9ac0be8d0
Sirene R Nova <12014557+SireneNova@users.noreply.github.com>
2022-02-16 17:54:15 -0800

    Update 4-PermutationMissingElem.py

--- Python/Version3+/Codility/4-PermutationMissingElem.py
+++ Python/Version3+/Codility/4-PermutationMissingElem.py
@@ -1,4 +1,4 @@
-# In a sequence of consecutive integers starting with 1, print the missing element. Assume there is one missing element. E.g. in in [3,5,1,2] the missing element is 4.
+# In a permutation of consecutive integers starting with 1, print the missing element. Assume there is one missing element. E.g. in in [3,5,1,2] the missing element is 4.
 # This solution got 100%. It took a few tries to realize that it wanted me to return a number above the maximum number in the sequence, which is what the last line handles. 
 # I don't think this question was stated very well.
 # This was another efficiency test, which I again handled with a dictionary.
