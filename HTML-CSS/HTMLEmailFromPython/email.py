a76d90d4edc66ac4689a2df856bdfe211871ff7a
Sirene R Nova <12014557+SireneNova@users.noreply.github.com>
2022-07-11 14:41:05 -0700

    Update email.py
    added ability to remove users from an opt out list

--- HTML-CSS/HTMLEmailFromPython/email.py
+++ HTML-CSS/HTMLEmailFromPython/email.py
@@ -9,14 +9,21 @@ Send emails from Python: Make sure you are loggged into TankExchange email
 import win32com.client
 import pandas as pd
 
-attachment = 'U:\Documents\TankExchange\Marketing\email template.html'
+# change attachment for new emails:
+attachment = 'U:\Documents\TankExchange\Marketing\emails\email1.html'
 with open(attachment, 'r') as myfile:
    data=myfile.read()
    
-#UsersExport-Tank_Exchange-2022-03-11_02_26_38.csv 
-userdf = pd.read_csv('test_users.csv', index_col=2)
-recipientNames = userdf["Name"]
-recipientList = userdf.index
+#UsersExport-Tank_Exchange-.....csv 
+userdf = pd.read_csv('UsersExport-Tank_Exchange-2022-07-11_19_19_22.csv', index_col=(2)) #index column is email column
+
+#remove users who don't want to be emailed
+optOutList = ["dummy1@email", "dummy2@email"]
+for item in optOutList:
+    userdf.drop(userdf.index[userdf.index == item], inplace = True)
+    
+recipientNames = userdf["Name"] #['Wrenn', 'Tank']
+recipientList = userdf.index #['sirene_nova@rl.gov', 'tankexchange@rl.gov']
 outlook = win32com.client.Dispatch('outlook.application')
 
 for recipient in recipientList:
@@ -48,14 +55,14 @@ for recipient in recipientList:
       <head>
       </head>
       <body topmargin="0" rightmargin="0" bottommargin="0" leftmargin="0" marginwidth="0" marginheight="0" width="100%" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; width: 100%; height: 100%; -webkit-font-smoothing: antialiased; text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; line-height: 100%;
-	background-color: #ffffff;
-	color: #000000;"
-	bgcolor="#ffffff"
-	text="#000000">
-         <table border="0" cellpadding="0" cellspacing="0" align="center"
-	bgcolor="#FFFFFF"
-	width="560" style="border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit;
-	max-width: 560px;" class="container">
+ 	background-color: #ffffff;
+ 	color: #000000;"
+ 	bgcolor="#ffffff"
+ 	text="#000000">
+          <table border="0" cellpadding="0" cellspacing="0" align="center"
+ 	bgcolor="#FFFFFF"
+ 	width="560" style="border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit;
+ 	max-width: 560px;" class="container">
             <tr>
         		<td align="left" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 17px; font-weight: 400; line-height: 160%;
          			padding-top: 25px; 
@@ -64,13 +71,13 @@ for recipient in recipientList:
         				Hi {0},
         		</td>
          	</tr>
-         </table>
+          </table>
       </body>
     </html>""".format(name)
     
        
     mail = outlook.CreateItem(0)
     mail.To = recipient
-    mail.Subject = 'Sample' #'What\'s your \"duh\" question?'
+    mail.Subject = 'Three important announcements' #'What\'s your \"duh\" question?'
     mail.HTMLBody = msg + data   
     mail.Send()
