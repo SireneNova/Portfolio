b4da089d421553cf44026fa55ac3dde702d38129
Sirene R Nova <12014557+SireneNova@users.noreply.github.com>
2022-07-26 17:04:30 -0700

    Update x13-MinAvgTwoSlice.py

--- Python/Version3+/Codility/x13-MinAvgTwoSlice.py
+++ Python/Version3+/Codility/x13-MinAvgTwoSlice.py
@@ -1,18 +1,19 @@
 # Find the minimum index that returns the slice of the array with the lowest average. This is in the prefix sums lesson.
 
 # This is a really difficult problem for a few reasons. It required understanding a new mathematical concept.
-# My orginal versions below returned the correct answer, but is not optimal for speed, so gives 60% O(N^2). It uses prefix sums. I thought the issue was the use of a nested loop, but removing it didn't help (see commented solution below).
+# My orginal versions below returned the correct answer, but were not optimal for speed, so gives 60% O(N^2). All versions use prefix sums. 
 # Since we want to find the lowest index, I made a function that finds the reversed prefix sum (Right to Left) of the given array rather that Left to Right. 
 # This way when the this reversed prefix array is traversed to find averages, it is easier to identify minimum indices that give minimum averages. It is hard to explain.
+# I originally thought the issue was the use of a nested loop, but removing it didn't help.
 # The trick to fixing the efficiency is the mathematical understanding that not every slice needs to be compared. Just the ones with lengths of 2 or 3. 
 # This is because the averages of longer slices are made up of the smaller slices of 2 or 3. My uncommented answer below got 100% score O(N) efficiency.
 
 A = [4,2,2,5,1,5,8] #1
 #A = [1,1,1,1,1,1,1] #0
 #A = [6,1,2,3,4,5,0] #1 
-#A = [6, -3, -5, 100, -1, -100]
+#A = [6, -3, -5, 100, -1, -100] #4
 #A = [-3, -5, -8, -4, -10] #2
-#A = [1,2,3,4,5,6,7]
+#A = [1,2,3,4,5,6,7] #1
 
 def reversedPrefixSum(A):
     lengthPlus = len(A) + 1
@@ -48,7 +49,7 @@ def solution(A):
 print(solution(A))
 
 #Original version checking every slice:
-#correct, 60%, n^2 efficiency:
+#correct, 60%, O(n^2) efficiency:
 #def solution(A):
 #    revPref = reversedPrefixSum(A)
 #    minIndex = 0
