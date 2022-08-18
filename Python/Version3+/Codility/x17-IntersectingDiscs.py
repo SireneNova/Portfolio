defdcedfc84d962fc346f1d7145d9b0af959a375
Sirene R Nova <12014557+SireneNova@users.noreply.github.com>
2022-08-18 15:38:38 -0700

    Update x17-IntersectingDiscs.py

--- Python/Version3+/Codility/x17-IntersectingDiscs.py
+++ Python/Version3+/Codility/x17-IntersectingDiscs.py
@@ -4,14 +4,16 @@
 # This is in the sorting section. It has been hard to see what kind of sorting would make this faster.
 # The function should return −1 if the number of intersecting pairs exceeds 10,000,000.
 
-# Here is a correct solution that scores 62% due to performance. The performance is O(N^2) but is improved 12% over an earlier version.
-# This solution is quite complicated. 
-# It creates disc objects out of the array, sorts them by top range, compares the overlap using a variable, and, if it needs to, compares using a dictionary.
+# Here is a correct solution that scores 68% due to performance. The performance is O(N^2) but is improved 18% over the first version.
+# This solution creates disc objects out of the array to keep track of their top and bottom, and then sorts them by bottom. 
+# It then iterates through the discs while keeping track of the tops as it goes and sorts this list of tops.
+# It compares the bottom of each disc to the list of tops. 
+# If the bottom of the disc is less than the minimum top, it that means it intersects with all discs below it, and the count can be increased by the position of the disc.
+# If not, the count can be set to the position minus the number of previous tops that are less than the disc bottom.
 # I usually avoid using Python for objects because it is awkward. There is more information entered into the discs than is needed, but I may experiment more with it.
 
 class disc:
-  def __init__(self, center, top, bot, radius):
-    self.center = center
+  def __init__(self, top, bot, radius):
     self.top = top
     self.bot = bot
     self.radius = radius
@@ -26,41 +28,40 @@ def solution(A):
     discs = []
 
     for i in range(length):
-        center = i
         radius = A[i]
         bot = i - radius
-        top = i + radius
+        top = i + radius               
 
-        the_disc = disc(center, top, bot, radius)
-
+        the_disc = disc(top, bot, radius)
         discs.append(the_disc)
     
-    discs.sort(key = lambda x:(x.top, x.radius))
-
-    minTop = discs[0].top
-    previousTops = {minTop:1}
+    discs.sort(key = lambda x:(x.bot, x.radius)) #sort by bottom, then radius. may not really need to sort by radius, but it is good to know it is possible to do.
 
     while count <= 10000000:
+
+        topsTracker = [discs[0].top]        
+
         for i in range(1, length, 1):
-            the_disc = discs[i]
-            top = the_disc.top
-
-            if the_disc.bot <= minTop:
-                #print("radius: " + str(discs[i].radius))
-                #print("center: " + str(the_disc.center))
-                count += i
-                #print(i)            
-            else:
-                for key, value in previousTops.items():
-                    if key >= the_disc.bot:
-                        count += value 
-                        #print("hit")
             
-            if top in previousTops:
-                previousTops[top] += 1
-            else:
-                previousTops[top] = 1
+            the_disc = discs[i]
+            
+            #This first if statement isn't really needed. It just makes it easier to explain the next part:
+            if the_disc.bot <= topsTracker[0]:
+                count += i       
+                topsTracker.append(the_disc.top)
+                topsTracker.sort()
 
+            else:
+                subtract = 0
+                for j in range(len(topsTracker)):
+                    if topsTracker[j] < the_disc.bot:
+                        subtract += 1
+                    else:
+                        count += (i-subtract)
+                        topsTracker.append(the_disc.top)
+                        topsTracker.sort()
+                        break         
+            
         return count
     
     return -1
@@ -108,3 +109,4 @@ A = [1, 4, 2, 8, 0, 3]
 #Progression:
 #https://app.codility.com/demo/results/training84G88R-8HZ/
 #https://app.codility.com/demo/results/training4VWHEH-5CM/
+#https://app.codility.com/demo/results/trainingYB42VU-7U2/
